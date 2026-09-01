015b2586a013f1edfb4e4f115f1464e069653187
Renan Borba Lucas <renanblucas@gmail.com>
2026-08-31 21:24:20 -0300

    chore: Add ktlint and format

--- app/src/test/java/com/nborba/vocalize/ExampleUnitTest.kt
+++ app/src/test/java/com/nborba/vocalize/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.nborba.vocalize
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
