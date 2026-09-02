f9539ef1ffd439fdd9ba5cdf3e078fe1a246b9b6
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-02 02:46:04 -0300

    feat: Add ArrowBack icon and update Check and Close to outlined style
    * Add `ArrowBack` icon to `VocalizeIcons`
    * Update `Check` and `Close` icons from Rounded to Outlined style

--- core/designsystem/src/main/kotlin/com/nborba/vocalize/core/designsystem/icon/VocalizeIcons.kt
+++ core/designsystem/src/main/kotlin/com/nborba/vocalize/core/designsystem/icon/VocalizeIcons.kt
@@ -1,14 +1,16 @@
 package com.nborba.vocalize.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.Close
 import androidx.compose.ui.graphics.vector.ImageVector
 
 object VocalizeIcons {
-    val Check: ImageVector = Icons.Rounded.Check
-    val Close: ImageVector = Icons.Rounded.Close
+    val ArrowBack: ImageVector = Icons.AutoMirrored.Outlined.ArrowBack
+    val Check: ImageVector = Icons.Outlined.Check
+    val Close: ImageVector = Icons.Outlined.Close
     val Settings: ImageVector = Icons.Outlined.Settings
     // Add vector drawables via R.drawable to add custom SVG assets
 }
