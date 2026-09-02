44794b54ada8e066279886ae7e448ede5a3b8e65
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-01 23:49:28 -0300

    feat: Add HomeScreen composable and UI tests
    * Add HomeScreen UI with top bar and navigation button
    * Add instrumented UI tests for HomeScreen content display and click handling

---
+++ app/src/main/java/com/nborba/vocalize/ui/compose/home/HomeScreen.kt
@@ -0,0 +1,66 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.nborba.vocalize.ui.compose.home
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import kotlin.random.Random
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    onNavigateToDetail: (String) -> Unit,
+) {
+    HomeContent(
+        modifier = modifier,
+        onNavigateToDetail = onNavigateToDetail,
+    )
+}
+
+@Composable
+private fun HomeContent(
+    modifier: Modifier = Modifier,
+    onNavigateToDetail: (String) -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Vocalize")
+                },
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = modifier.padding(innerPadding),
+        ) {
+            Text(text = "Welcome to the app!")
+            Button(onClick = { onNavigateToDetail(Random.nextInt().toString()) }) {
+                Text("See details")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeContentPreview() {
+    val context = LocalContext.current
+    val toast: (String) -> Unit = { message ->
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    HomeContent(
+        onNavigateToDetail = { toast("onNavigateToDetail") },
+    )
+}
