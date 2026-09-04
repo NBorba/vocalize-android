e2e04f9fe05869c69db414d9a7872fe2310cd324
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-04 18:06:48 -0300

    test: Add UI tests for recorder navigation

--- app/src/androidTest/java/com/nborba/vocalize/navigation/VocalizeNavigationTest.kt
+++ app/src/androidTest/java/com/nborba/vocalize/navigation/VocalizeNavigationTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.espresso.Espresso
 import com.nborba.vocalize.core.designsystem.theme.VocalizeTheme
 import com.nborba.vocalize.ui.VocalizeApp
 import org.junit.Rule
@@ -33,6 +34,30 @@ class VocalizeNavigationTest {
 
         // 4. Click TopBar Back Arrow to navigate back to HomeScreen
         composeTestRule.onNodeWithContentDescription("Back").performClick()
+
+        // 5. Verify Home Screen is displayed again
+        composeTestRule.onNodeWithText("Welcome to the app!").assertIsDisplayed()
+    }
+
+    @Test
+    fun fullNavigationFlow_homeToRecorderAndBack() {
+        composeTestRule.setContent {
+            VocalizeTheme {
+                VocalizeApp()
+            }
+        }
+
+        // 1. Verify Home Screen is displayed
+        composeTestRule.onNodeWithText("Welcome to the app!").assertIsDisplayed()
+
+        // 2. Click "Record" to navigate to Recorder
+        composeTestRule.onNodeWithText("Record").performClick()
+
+        // 3. Verify Recorder is displayed
+        composeTestRule.onNodeWithText("Recorder").assertIsDisplayed()
+
+        // 4. Press back to dismiss
+        Espresso.pressBack()
 
         // 5. Verify Home Screen is displayed again
         composeTestRule.onNodeWithText("Welcome to the app!").assertIsDisplayed()
