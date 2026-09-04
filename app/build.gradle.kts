9cd86d3c071f9e3396e3cf0adf83bf8574d863d6
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-04 18:24:41 -0300

    build(convention): Add testing dependencies to feature convention plugin
    * Add JUnit, Espresso, Compose UI test, and test manifest dependencies to `AndroidFeatureConventionPlugin`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.compose.material.navigation)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.hilt.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -26,6 +26,9 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 // Design System dependency for all feature modules
                 implementation(project(":core:designsystem"))
 
+                ksp(libs.findLibrary("hilt.compiler").get())
+                implementation(libs.findLibrary("hilt.android").get())
+
                 implementation(platform(libs.findLibrary("androidx.compose.bom").get()))
                 implementation(libs.findLibrary("androidx.activity.compose").get())
                 implementation(libs.findLibrary("androidx.compose.material.navigation").get())
@@ -34,10 +37,16 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 implementation(libs.findLibrary("androidx.compose.ui.tooling.preview").get())
                 implementation(libs.findLibrary("androidx.hilt.navigation.compose").get())
                 implementation(libs.findLibrary("androidx.navigation.compose").get())
-                debugImplementation(libs.findLibrary("androidx.compose.ui.tooling").get())
 
-                implementation(libs.findLibrary("hilt.android").get())
-                ksp(libs.findLibrary("hilt.compiler").get())
+                testImplementation(libs.findLibrary("junit").get())
+                androidTestImplementation(platform(libs.findLibrary("androidx.compose.bom").get()))
+                androidTestImplementation(libs.findLibrary("androidx.compose.ui.test.junit4").get())
+                androidTestImplementation(libs.findLibrary("androidx.espresso.core").get())
+                androidTestImplementation(libs.findLibrary("androidx.junit").get())
+                debugImplementation(libs.findLibrary("androidx.compose.ui.tooling").get())
+                debugImplementation(libs.findLibrary("androidx.compose.ui.test.manifest").get())
+
+
             }
         }
     }
