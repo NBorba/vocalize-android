71846193f64447a33fa7af9c82f3059690246f13
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-08 22:44:16 -0300

    feat: Add effect handling and user actions to HomeScreenViewModel

--- app/src/main/kotlin/com/nborba/vocalize/ui/home/HomeScreenViewModel.kt
+++ app/src/main/kotlin/com/nborba/vocalize/ui/home/HomeScreenViewModel.kt
@@ -2,11 +2,14 @@ package com.nborba.vocalize.ui.home
 
 import androidx.lifecycle.ViewModel
 import com.nborba.vocalize.ui.home.mapper.HomeUiStateMapper
+import com.nborba.vocalize.ui.home.model.HomeEffect
 import com.nborba.vocalize.ui.home.model.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,4 +20,18 @@ internal class HomeScreenViewModel
     ) : ViewModel() {
         private val _uiState = MutableStateFlow(homeUiStateMapper())
         val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+        val effects = uiState.map { it.effect }
+
+        fun onRecordButtonClick() {
+            _uiState.update { it.copy(effect = HomeEffect.NavigateToRecorder) }
+        }
+
+        fun onDetailItemClick(id: String) {
+            _uiState.update { it.copy(effect = HomeEffect.NavigateToDetail(id)) }
+        }
+
+        fun onEventHandled() {
+            _uiState.update { it.copy(effect = null) }
+        }
     }
