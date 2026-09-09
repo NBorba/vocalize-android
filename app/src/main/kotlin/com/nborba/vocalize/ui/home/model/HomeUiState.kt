22fc71cd036df2b56904e1e0dbc5aa4f551a97f1
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-08 22:43:15 -0300

    feat: Add HomeEffect and effect property to HomeUiState

--- app/src/main/kotlin/com/nborba/vocalize/ui/home/model/HomeUiState.kt
+++ app/src/main/kotlin/com/nborba/vocalize/ui/home/model/HomeUiState.kt
@@ -1,8 +1,17 @@
 package com.nborba.vocalize.ui.home.model
 
+internal sealed interface HomeEffect {
+    data object NavigateToRecorder : HomeEffect
+
+    data class NavigateToDetail(
+        val id: String,
+    ) : HomeEffect
+}
+
 internal data class HomeUiState(
     val title: String,
     val header: String,
     val buttonDetails: String,
     val buttonRecord: String,
+    val effect: HomeEffect? = null,
 )
