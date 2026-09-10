76771be64f285b20a23af501593a10aba58eb475
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-09 22:42:01 -0300

    feat(recorder): Add RecorderUiState and RecorderUiStateMapper
    * Add `RecorderUiState` model class
    * Add `RecorderUiStateMapper` to map string resources to UI state

---
+++ feature/recorder-impl/src/main/kotlin/com/nborba/vocalize/feature/recorder/impl/ui/recorder/mapper/RecorderUiStateMapper.kt
@@ -0,0 +1,17 @@
+package com.nborba.vocalize.feature.recorder.impl.ui.recorder.mapper
+
+import com.nborba.vocalize.core.common.util.StringProvider
+import com.nborba.vocalize.feature.recorder.impl.R
+import com.nborba.vocalize.feature.recorder.impl.ui.recorder.model.RecorderUiState
+import javax.inject.Inject
+
+internal class RecorderUiStateMapper
+    @Inject
+    constructor(
+        private val stringProvider: StringProvider,
+    ) {
+        operator fun invoke(): RecorderUiState =
+            RecorderUiState(
+                title = stringProvider.getString(R.string.recorder_title),
+            )
+    }
