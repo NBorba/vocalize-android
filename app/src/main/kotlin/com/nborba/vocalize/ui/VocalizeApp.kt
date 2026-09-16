5ebef72b3d31dee4e6ae0089e3e69fac70889412
Renan Borba Lucas <renanblucas@gmail.com>
2026-09-16 05:28:14 -0300

    refactor(navigation): Remove legacy bottom sheet navigator and update recorderNavGraph

--- app/src/main/kotlin/com/nborba/vocalize/ui/VocalizeApp.kt
+++ app/src/main/kotlin/com/nborba/vocalize/ui/VocalizeApp.kt
@@ -1,10 +1,6 @@
-@file:Suppress("UsingMaterialAndMaterial3Libraries")
-
 package com.nborba.vocalize.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.material.navigation.ModalBottomSheetLayout
-import androidx.compose.material.navigation.rememberBottomSheetNavigator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,28 +15,27 @@ import com.nborba.vocalize.ui.home.compose.HomeScreen
 
 @Composable
 internal fun VocalizeApp() {
-    val bottomSheetNavigator = rememberBottomSheetNavigator()
-    val navController = rememberNavController(bottomSheetNavigator)
+    val navController = rememberNavController()
 
-    ModalBottomSheetLayout(bottomSheetNavigator) {
-        NavHost(
-            navController = navController,
-            startDestination = HomeRoute,
-            modifier = Modifier.background(MaterialTheme.colorScheme.background),
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoute,
+        modifier = Modifier.background(MaterialTheme.colorScheme.background),
+    ) {
+        composable<HomeRoute>(
+            deepLinks =
+                listOf(
+                    navDeepLink<HomeRoute>(basePath = "https://vocalize.app"),
+                    navDeepLink<HomeRoute>(basePath = "vocalize://app"),
+                ),
         ) {
-            composable<HomeRoute>(
-                deepLinks =
-                    listOf(
-                        navDeepLink<HomeRoute>(basePath = "https://vocalize.app"),
-                        navDeepLink<HomeRoute>(basePath = "vocalize://app"),
-                    ),
-            ) {
-                HomeScreen(
-                    onNavigateToRecorder = { navController.navigate(RecorderGraphRoute) },
-                )
-            }
-
-            recorderNavGraph()
+            HomeScreen(
+                onNavigateToRecorder = { navController.navigate(RecorderGraphRoute) },
+            )
         }
+
+        recorderNavGraph(
+            onDismissRequest = { navController.popBackStack() },
+        )
     }
 }
